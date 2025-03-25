62b6ca0e0a2be31c20ee8f4c1489ca486e7ad051
Santi Figueiras <santi.figueiras95@gmail.com>
2025-03-25 16:24:53 -0300

    Working on player, audio is working

--- src/app/components/Player.tsx
+++ src/app/components/Player.tsx
@@ -3,25 +3,160 @@ import * as Tone from "tone";
 import { useState } from "react";
 
 const Player = () => {
-  const [player, setPlayer] = useState<Tone.Player | null>(null);
+  // const [player, setPlayer] = useState<Tone.Player | null>(null);
+  const [mute, setMute] = useState<Tone.Player | null>(null);
+  
+  const onVolumeChange = () => {
+    debugger;
+    const toneDestination = Tone.getDestination();
+    // toneDestination.mute = controls.muted;
+    // toneDestination.output.gain.value = controls.volume;
+  }
 
-  const loadSample = async () => {
-    const newPlayer = new Tone.Player("/samples/vsco2-ce/upright-piano/a0.wav").toDestination();
-    setPlayer(newPlayer);
+  const onPlay = async () => {
+    await Tone.start();
+    const transport = Tone.getTransport().start();
+
+    const Entusiasmo = await import('./pieces/entusiasmo');
+
+    await Tone.loaded();
+
+    console.log(transport.state)
+
+    console.log(Entusiasmo);
+    // document.querySelector('#tone-load-feedback')!.innerText =
+    //   'Audio files loaded!';
+    // drawAnalysers(registeredLFOs);
+  }
+
+  const onPause = () => {
+    Tone.getTransport().stop();
+    Tone.getDestination().mute = true;
+
+    // For now, force student to reload page after stopping
+    // so we don't have to deal with disposing nodes and weird "restart" behavior
+    const controlsContainer = document.getElementById('controls-container')!;
+    // controlsContainer.removeChild(controls);
+
+    const reloadButton = document.createElement('button');
+    reloadButton.innerText = 'Refresh';
+
+    reloadButton.addEventListener('click', () => {
+      window.location.reload();
+    });
+
+    controlsContainer.append(reloadButton);
+  }
+
+  const AUTO_PLAYBACK_KEY_NAME = 'InfinitePieces.Settings.PlayAutomatically';
+
+  const handleRestartBehavior = () => {
+    const shouldPlayAutomatically =
+      localStorage.getItem(AUTO_PLAYBACK_KEY_NAME) === 'true';
+    const automaticPlaybackCheckbox = document.getElementById(
+      'automatic-play-checkbox'
+    ) as HTMLInputElement;
+    if (shouldPlayAutomatically) {
+      automaticPlaybackCheckbox.checked = true;
+      const controls = document.getElementById('controls') as HTMLAudioElement;
+      controls.play();
+    }
+    automaticPlaybackCheckbox.addEventListener('change', () => {
+      localStorage.setItem(
+        AUTO_PLAYBACK_KEY_NAME,
+        automaticPlaybackCheckbox.checked.toString()
+      );
+    });
   };
 
-  const playSample = () => {
-    if (player) {
-      player.start();
+  const assignVersion = () => {
+    const versionTag = document.getElementById(
+      'version-link'
+    ) as HTMLAnchorElement;
+    versionTag.innerText = `Tone.js version ${Tone.version}`;
+    versionTag.href = `https://tonejs.github.io/docs/${Tone.version}/`;
+  };
+
+  // Extract: Anaylzer Plotting
+  // Como podemos acceder a los tipos de Tone
+  const drawAnalysers = (registeredLFOs: any) => {
+
+    function draw(ctx: any, canvas: any, analyser: any) {
+      requestAnimationFrame(() => draw(ctx, canvas, analyser));
+      const values = analyser.getValue();
+
+      // Limpiar canvas
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.beginPath();
+      ctx.strokeStyle = '#ff0077';
+      ctx.lineWidth = 2;
+
+      // Dibujar la forma de onda
+      for (let i = 0; i < values.length; i++) {
+        const x = (i / values.length) * canvas.width;
+        const y = (1 - (values[i] +1) / 2) * canvas.height;
+        if (i === 0) {
+          ctx.moveTo(x, y );
+        } else {
+          ctx.lineTo(x, y);
+        }
+      }
+
+      ctx.stroke();
     }
+
+    const lfoContainer = document.getElementById("lfo-container")!;
+    registeredLFOs.length > 0
+    && lfoContainer.appendChild(document.createTextNode("LFOs")) 
+    && lfoContainer.appendChild(document.createElement("br"))
+    && lfoContainer.appendChild(document.createElement("br"));
+
+    registeredLFOs.forEach((registeredLFO: any, i: number) => {
+      // Configurar canvas
+      const canvas = document.createElement("canvas");
+      canvas.setAttribute("id", `anaylser-node-${i}`);
+      canvas.setAttribute("style", "background-color: white");
+
+      lfoContainer.appendChild(document.createTextNode(registeredLFO.name));
+      lfoContainer.appendChild(canvas);
+
+      const ctx = canvas.getContext("2d");
+      canvas.width = 200;
+      canvas.height = 200;
+
+      draw(ctx, canvas, registeredLFO.analyser);
+    });
+  }; 
+
+  const onLoad = () => {
+    console.log('DOMloaded')
+    assignVersion();
+    handleRestartBehavior();
   };
 
   return (
-    <div>
-      <button onClick={loadSample}>Cargar Sample</button>
-      <button onClick={playSample} disabled={!player}>
-        Reproducir
-      </button>
+    <div onLoad={onLoad}>
+      <section id="controls-container">
+        <audio
+          id="controls"
+          controls
+          loop
+          src="/silence.mp3"
+          onPlay={onPlay}
+          onPause={onPause}
+          onVolumeChange={onVolumeChange}
+        ></audio>
+      </section>
+      <section>
+        <input
+          type="checkbox"
+          id="automatic-play-checkbox"
+          name="automatic-play-checkbox"
+        />
+        <label htmlFor="automatic-play-checkbox">Try to play automatically on page load</label>
+      </section>
+      <p id="tone-load-feedback"></p>
+      <section id="lfo-container" />
     </div>
   );
 };
---
+++ src/app/components/pieces/entusiasmo.ts
@@ -0,0 +1,232 @@
+import * as Tone from 'tone';
+
+const samplerConfig = {
+    samples: {
+        baseUrl: '/samples/vsco2-ce/upright-piano/',
+        urls: {
+          "A0": "a0.wav",
+          "C#1": "csharp1.wav",
+          "F1": "f1.wav",
+          "C#2": "csharp2.wav",
+          "F2": "f2.wav",
+          "A2": "a2.wav",
+          "C#3": "csharp3.wav",
+          "F3": "f3.wav",
+          "A3": "a3.wav",
+          "C#4": "csharp4.wav",
+          "F4": "f4.wav",
+          "A4": "a4.wav",
+          "C#5": "csharp5.wav",
+          "F5": "f5.wav",
+          "A5": "a5.wav",
+          "C#6": "csharp6.wav",
+          "F6": "f6.wav",
+          "A6": "a6.wav",
+          "C#7": "csharp7.wav",
+          "F7": "f7.wav",
+          "A7": "a7.wav",
+          "C8": "c8.wav"
+        },
+    },
+  onload: function () {
+    Tone.start();
+  },
+};
+
+const uprightMap = {
+    "A0": "a0.wav",
+    "C#1": "csharp1.wav",
+    "F1": "f1.wav",
+    "C#2": "csharp2.wav",
+    "F2": "f2.wav",
+    "A2": "a2.wav",
+    "C#3": "csharp3.wav",
+    "F3": "f3.wav",
+    "A3": "a3.wav",
+    "C#4": "csharp4.wav",
+    "F4": "f4.wav",
+    "A4": "a4.wav",
+    "C#5": "csharp5.wav",
+    "F5": "f5.wav",
+    "A5": "a5.wav",
+    "C#6": "csharp6.wav",
+    "F6": "f6.wav",
+    "A6": "a6.wav",
+    "C#7": "csharp7.wav",
+    "F7": "f7.wav",
+    "A7": "a7.wav",
+    "C8": "c8.wav"
+};
+
+const sampler = new Tone.Sampler(
+    uprightMap,
+    samplerConfig.onload,
+    '/samples/vsco2-ce/upright-piano/'
+);
+
+const samplerUntouched = new Tone.Sampler(
+    uprightMap,
+    samplerConfig.onload,
+    '/samples/vsco2-ce/upright-piano/'
+);
+
+/**
+ * Setup
+ */
+// const melodia = ['F4', 'Ab4', 'C5', 'Db5', 'Eb5', 'F5', 'Ab5'];
+const melodia = ['C4', 'E4', 'G4', 'B4', 'C5'];
+const texturasA = ['D6', 'F#6', 'A6'];
+const texturasB = ['C6', 'E6', 'A6'];
+
+const bajosA = ['C2', 'G2'];
+const bajosB = ['C2', 'A1'];
+// const subBajos = ['C0', 'F0'];
+
+// const repeticiones = [23.53, 25.76, 29.123, 19.29, 16.831, 22.432, 27.97];
+// const duraciones = [0, 5.23, 20.91, 15.47, 9.77, 19.55, 20.112];
+
+melodia.forEach(function (nota, i) {
+  scheduleRandomRepeat(function(time) {
+    sampler.triggerAttack(nota, time);
+  }, 10, 0, getRandomBetween(0, 5));
+});
+
+texturasA.forEach(function (nota, i) {
+  scheduleRandomRepeat(function(time) {
+    sampler.triggerAttack(nota, time);
+  }, 60, 0, getRandomBetween(0, 5));
+});
+
+texturasB.forEach(function (nota, i) {
+  scheduleRandomRepeat(function(time) {
+    sampler.triggerAttack(nota, time);
+  }, 120, 60, getRandomBetween(0, 5));
+});
+
+bajosA.forEach(function (nota, i) {
+  scheduleRandomRepeat(function(time) {
+    samplerUntouched.triggerAttack(nota, time);
+  }, 60, 0, getRandomBetween(0, 5));
+});
+
+bajosB.forEach(function (nota, i) {
+  scheduleRandomRepeat(function(time) {
+    samplerUntouched.triggerAttack(nota, time);
+  }, 120, 60, getRandomBetween(0, 1));
+});
+
+/**
+ * Modular bajos solo cada cierta cantidad de tiempo
+ */
+
+// subBajos.forEach(function (nota, i) {
+//   scheduleRandomRepeat(function(time) {
+//     samplerUntouched.triggerAttack(nota, time);
+//   }, 60, 0, getRandomBetween(0, 20));
+// });
+
+
+function getRandomBetween(min: number, max: number) {
+  return Math.random() * (max - min) + min;
+}
+
+function scheduleRandomRepeat(
+    scheduledFunction: (time: number) => void,
+    minDelay: number,
+    maxDelay:number, 
+    startTime = getRandomBetween(minDelay, maxDelay)
+) {
+  Tone.Transport.scheduleOnce(function(time: number) {
+    scheduledFunction(time);
+    const delay = getRandomBetween(minDelay, maxDelay);
+    scheduleRandomRepeat(scheduledFunction, minDelay, maxDelay, time + delay);
+  }, startTime);
+}
+
+/**
+ * Effects
+ */
+const freeverb = new Tone.Freeverb({ roomSize: .3, dampening: 2000, wet: .8});
+const pingPong = new Tone.PingPongDelay({feedback: 0.2, wet: 0.8});
+const dist = new Tone.Distortion(0.3);
+const EQ = new Tone.EQ3({highFrequency: 8000, high: -12});
+
+/**
+ * Extract: Analysers
+ */
+
+interface LFOAnalyser {
+    analyser: Tone.Analyser;
+    lfo: Tone.LFO;
+    name: string;
+}
+
+export const registeredLFOs: LFOAnalyser[] = [];
+
+function registerLFO (name: string, lfo: Tone.LFO) {
+  registeredLFOs.push({
+    name,
+    analyser: createAnalyser(lfo),
+    lfo
+  });
+}
+
+function createAnalyser (targetLfo: Tone.LFO) {
+  const analyser = new Tone.Analyser("waveform")
+  targetLfo.connect(analyser);
+  return analyser;
+} 
+
+/**
+ * LFOs
+ */
+// const delayLFO = new Tone.LFO({frequency: 0.5, min: 0, max: 0.5});
+const delayLFO = new Tone.LFO({min: 0, max: 1});
+delayLFO.connect(pingPong.delayTime);
+delayLFO.start();
+
+const delayFrequencyLFO = new Tone.LFO({frequency: 0.001, min: 0, max: 0.001});
+delayFrequencyLFO.connect(delayLFO.frequency); 
+delayFrequencyLFO.start();
+
+const samplerVolumeLFO = new Tone.LFO({ type: "square", frequency: 0.001, min: -16, max: 0 });
+samplerVolumeLFO.connect(sampler.volume);
+samplerVolumeLFO.start();
+
+const structureLFO = new Tone.LFO({frequency: 0.5, min: 0, max: 1});
+structureLFO.connect(pingPong.wet);
+structureLFO.start();
+
+registerLFO('Delay Frequency', delayFrequencyLFO);
+registerLFO('Sampler Volume', samplerVolumeLFO);
+registerLFO('Structural', structureLFO);
+
+/**
+ * TODO:
+ * 
+ * Separar alguna cada que no este atada al delay para dar profundidad
+ * Agregar samples de voces
+ * Utilizar LFOs a nivel estructural de la pieza, frecuencias muy bajas
+ * para realizar evoluciones muy lentas en la pieza.
+ * 
+ * Reflexiones: la forma de una pieza se puede representar en contraste mediante
+ * una forma de onda cuadrada. Dentro de la forma el crescendo, o la acumu
+ * lación de las transformaciones en los parametros se puede representar
+ * con una onda diente de sierra.
+ * Combinando ambas se pueden crear macro estructuras de piezas a partir
+ * de la definición de bloques fundamentales desarrollados en el tiempo
+ * con LFOs.
+ */
+
+console.log('All loaded');
+
+/**
+ * Sends
+ */
+sampler.connect(pingPong);
+pingPong.connect(freeverb);
+freeverb.connect(EQ)
+EQ.toDestination();
+
+// dist.connect(samplerUntouched);
+samplerUntouched.toDestination();
