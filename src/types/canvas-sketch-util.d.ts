0d43d05ccd75da76dfb916fdff0e3062fde0ffac
Santi Figueiras <santi.figueiras95@gmail.com>
2025-03-31 15:02:29 -0300

    Add random seed

--- src/types/canvas-sketch-util.d.ts
+++ src/types/canvas-sketch-util.d.ts
@@ -1,5 +1,7 @@
 declare module 'canvas-sketch-util/random' {
   const random: {
+    getRandomSeed(): number;
+    setSeed(arg0: number | string): void;
     range(min: number, max: number): number;
     pick<T>(array: T[]): T;
     value(): number;
