0aa26aa7b91a612d673953907df2aa772734bf81
Santiago Figueiras <santi.figueiras95@gmail.com>
2025-05-09 15:59:13 -0300

    Added Storybook setup

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,14 @@
+/**
+ * Change the auto-generated file name to `postcss.config.mjs`
+ * given the issue with Storybook.
+ *
+ * The resolution is pointed here:"
+ * https://github.com/storybookjs/storybook/issues/30208#issuecomment-2830668556
+ */
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: {
+    "@tailwindcss/postcss": {},
+  },
 };
 
 export default config;
